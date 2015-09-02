2f51bd0d62baf6c34fa12727c53a34f3a2edac6a
mafernando <fernando_michael@bah.com>
2015-09-02 17:53:32 -0500

    Update Simple Listener Spec
    Pass a Project OpenStruct Mock object to the listener to use for
    processing. Project mock as a JSON string which will be parsed into a
    hash to simulate response from Project controller in JF api.
    Changed broadcast to publish to reflect current syntax and passed it an
    empty string to invoke Action Mailer configuration settings initialized
    for the test to pull up a dummy to and from address. Also abstracted
    out what happens when mail is delivered asynchronously. Need to figure
    out how to better test asynchronous mail with delayed jobs.

--- spec/lib/jellyfish_notification/simple_listener_spec.rb
+++ spec/lib/jellyfish_notification/simple_listener_spec.rb
@@ -1,27 +1,29 @@
 class Project
   include Wisper::Publisher
 
-  def self.instance_params
-    OpenStruct.new(id: 1,
-                   name: 'foo',
-                   description: nil,
-                   cc: nil,
-                   staff_id: nil,
-                   img: nil,
-                   created_at: "#{Time.zone.now}",
-                   updated_at: "#{Time.zone.now}",
-                   deleted_at: nil,
-                   status: 0,
-                   approval: 0,
-                   archived: nil,
-                   spent: BigDecimal(0.0, 2),
-                   budget: BigDecimal(1.0, 2),
-                   start_date: "#{Time.zone.now}",
-                   end_date: "#{Time.zone.now + 7.days}")
+  def self.mock
+    # MOCK JSON RESPONSE FROM THE JF PROJECT CONTROLLER
+    OpenStruct.new(body:  "{
+                   \"id\": 1,
+                   \"name\": \"foo\",
+                   \"description\": null,
+                   \"cc\": null,
+                   \"staff_id\": null,
+                   \"img\": null,
+                   \"created_at\": \"#{Time.zone.now}\",
+                   \"updated_at\": \"#{Time.zone.now}\",
+                   \"deleted_at\": null,
+                   \"status\": 0,
+                   \"approval\": 0,
+                   \"archived\": null,
+                   \"spent\": #{BigDecimal(0.0, 2)},
+                   \"budget\": #{BigDecimal(1.0, 2)},
+                   \"start_date\": \"#{Time.zone.now}\",
+                   \"end_date\": \"#{Time.zone.now + 7.days}\"}")
   end
 
   def save
-    broadcast('publish_project_creation', Project.instance_params)
+    publish('publish_project_creation', Project.mock, '')
   end
 end
 
@@ -31,7 +33,7 @@ describe JellyfishNotification::SimpleListener do
   end
 
   context 'project created' do
-    it 'should broadcast creation and send admin notification' do
+    it 'should publish project creation to send admin notification' do
       # CREATE DUMMY LISTENER - FOLLOWING WISPER RSPEC EXAMPLE AT https://github.com/krisleech/wisper/blob/master/spec/lib/integration_spec.rb
       listener = double('listener')
 
@@ -50,11 +52,16 @@ describe JellyfishNotification::SimpleListener do
       # SAVE PROJECT TO TRIGGER PROJECT CREATE NOTIFICATION
       project.save
 
-      # VERIFY THAT A MAIL WAS SENT UPON PROJECT SAVE AFTER INITIAL CREATE
-      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      if ENV['JELLYFISH_ASYNCHRONOUS_DELIVERY'] == 'true'
+        # VERIFY THAT A MAIL WAS SENT UPON PROJECT SAVE AFTER INITIAL CREATE
+        expect(ActionMailer::Base.deliveries.count).to eq(0)
+      else
+        # VERIFY THAT A MAIL WAS SENT UPON PROJECT SAVE AFTER INITIAL CREATE
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
 
-      # VERIFY THAT THE SENDER IS THE RECIPIENT SPECIFIED IN DOTENV FILE
-      expect(ActionMailer::Base.deliveries.last.from.first).to eq(JellyfishNotification::JellyfishMailer.default_params[:from])
+        # VERIFY THAT THE SENDER IS THE RECIPIENT SPECIFIED IN DOTENV FILE
+        expect(ActionMailer::Base.deliveries.last.from.first).to eq(JellyfishNotification::JellyfishMailer.default_params[:from])
+      end
     end
   end
 end
