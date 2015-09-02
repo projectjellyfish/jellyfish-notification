860588631e9ff998a65fa62e8cfc3a09d96b8fff
mafernando <fernando_michael@bah.com>
2015-09-02 17:48:44 -0500

    Refactor mailer response to project create ________________________________________________________________________ Change expectation that response will be a proper controller object. Am passing an OpenStruct with controller response body from JF api. Also added recipients, a string parameter, to the method. ActionView::Helpers are added to all number_to_currency method in email.

--- lib/jellyfish_notification/jellyfish_mailer.rb
+++ lib/jellyfish_notification/jellyfish_mailer.rb
@@ -1,11 +1,14 @@
 module JellyfishNotification
+  include ActionView::Helpers
+
   class JellyfishMailer < ActionMailer::Base
     default from: ENV['JELLYFISH_SMTP_DEFAULT_SENDER']
 
-    def publish_project_creation(project)
+    def publish_project_creation(response, recipients)
       # format block causes weirdness, so depending on rails conventions so templates resolve
-      @project = project
-      mail(template_path: 'jellyfish_mailer', to: ENV['JELLYFISH_SMTP_DEFAULT_RECIPIENT'], subject: "Project  #{project.name.to_s.upcase} Creation Notification")
+      @project = JSON.parse(response.body).to_h
+      recipients = ENV['JELLYFISH_SMTP_DEFAULT_RECIPIENT'] if recipients.empty?
+      mail(template_path: 'jellyfish_mailer', to: recipients, subject: "Project Creation Notification: #{@project['name'].to_s.upcase}")
     end
   end
 end
