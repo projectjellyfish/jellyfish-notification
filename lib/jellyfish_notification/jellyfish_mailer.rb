776fc1bb5316897b3de94f90b9e0cf718cd25a59
mafernando <fernando_michael@bah.com>
2015-09-09 16:19:51 -0500

    Use suggested rubocop syntax for conditionals

--- lib/jellyfish_notification/jellyfish_mailer.rb
+++ lib/jellyfish_notification/jellyfish_mailer.rb
@@ -7,7 +7,7 @@ module JellyfishNotification
     def publish_project_create_approvers(response, recipients, projects_url)
       # format block causes weirdness, so depending on rails conventions so templates resolve
       @project = JSON.parse(response.body).to_h
-      @project_url = projects_url+"/#{@project['id'].to_s}"
+      @project_url = projects_url + "/#{@project['id']}"
       recipients = ENV['JELLYFISH_SMTP_DEFAULT_RECIPIENT'] if recipients.empty?
       mail(template_path: 'jellyfish_mailer', to: recipients, subject: "Project Create Notification: #{@project['name'].to_s.upcase}") unless ENV['JELLYFISH_SMTP_DEFAULT_SENDER'].nil?
     end
@@ -15,7 +15,7 @@ module JellyfishNotification
     def publish_project_create_confirmation(response, recipients, projects_url)
       # format block causes weirdness, so depending on rails conventions so templates resolve
       @project = JSON.parse(response.body).to_h
-      @project_url = projects_url+"/#{@project['id'].to_s}"
+      @project_url = projects_url + "/#{@project['id']}"
       recipients = ENV['JELLYFISH_SMTP_DEFAULT_RECIPIENT'] if recipients.empty?
       mail(template_path: 'jellyfish_mailer', to: recipients, subject: "Project Create Notification: #{@project['name'].to_s.upcase}") unless ENV['JELLYFISH_SMTP_DEFAULT_SENDER'].nil?
     end
@@ -30,7 +30,7 @@ module JellyfishNotification
 
     def publish_order_create(order, recipients, orders_url)
       @order = order
-      @order_url = orders_url+"/#{@order.id}"
+      @order_url = orders_url + "/#{@order.id}"
       recipients = ENV['JELLYFISH_SMTP_DEFAULT_RECIPIENT'] if recipients.empty?
       mail(template_path: 'jellyfish_mailer', to: recipients, subject: 'Order Create Notification') unless ENV['JELLYFISH_SMTP_DEFAULT_SENDER'].nil?
     end
